0c8c7df7bb128b33a948a85028965b22e6a54f60
John Melody Me <Johnmelodyme@yandex.com>
2021-07-25 23:19:15 +0800

    added reload

--- app/src/main/java/uk/co/johnmelodyme/browser/activity/BrowserWebViewActivity.java
+++ app/src/main/java/uk/co/johnmelodyme/browser/activity/BrowserWebViewActivity.java
@@ -1,5 +1,6 @@
 package uk.co.johnmelodyme.browser.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -9,6 +10,8 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -17,6 +20,8 @@ import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 import uk.co.johnmelodyme.browser.R;
 import uk.co.johnmelodyme.browser.constant.LogLevel;
@@ -98,11 +103,11 @@ public class BrowserWebViewActivity extends AppCompatActivity
         /* Render User Interface Components */
         render_user_interface_components(savedInstanceState);
 
-        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        Objects.requireNonNull(this.getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.action_bar);
         getSupportActionBar().setElevation(0);
-        View view=getSupportActionBar().getCustomView();
+        View view = getSupportActionBar().getCustomView();
 
         view.findViewById(R.id.refresh).setOnClickListener(new View.OnClickListener()
         {
@@ -166,4 +171,60 @@ public class BrowserWebViewActivity extends AppCompatActivity
         return super.onKeyDown(keyCode, event);
     }
 
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.
+     *
+     * <p>This is only called once, the first time the options menu is
+     * displayed.  To update the menu every time it is displayed, see
+     * {@link #onPrepareOptionsMenu}.
+     *
+     * <p>The default implementation populates the menu with standard system
+     * menu items.  These are placed in the {@link Menu#CATEGORY_SYSTEM} group so that
+     * they will be correctly ordered with application-defined menu items.
+     * Deriving classes should always call through to the base implementation.
+     *
+     * <p>You can safely hold on to <var>menu</var> (and any items created
+     * from it), making modifications to it as desired, until the next
+     * time onCreateOptionsMenu() is called.
+     *
+     * <p>When you add items to the menu, you can implement the Activity's
+     * {@link #onOptionsItemSelected} method to handle them there.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     *
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.item, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.refresh:
+            {
+                webView.reload();
+                break;
+            }
+
+            default:
+            {
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
